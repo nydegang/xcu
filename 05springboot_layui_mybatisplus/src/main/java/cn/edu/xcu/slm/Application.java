a5c0b111e40728498b392f6fe52c596c0a145e7f
hp <hp@DESKTOP-T01PK4E>
2019-11-08 20:40:04 +0800

    添加了注释

--- 05springboot_layui_mybatisplus/src/main/java/cn/edu/xcu/slm/Application.java
+++ 05springboot_layui_mybatisplus/src/main/java/cn/edu/xcu/slm/Application.java
@@ -2,7 +2,7 @@ package cn.edu.xcu.slm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//这是SpringBoot的主类
 @SpringBootApplication
 public class Application {
 
